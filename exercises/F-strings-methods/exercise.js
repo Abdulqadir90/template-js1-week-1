4b5e2b4b2953eab2ea79ec17b0d5c6fd069a9121
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-22 22:43:24 +0100

    string methods

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+let name = "Daniel";
+let nameL = name.length;
+let message = "My name is " + name + " and my name is " + nameL + " characters long ";
 
 console.log(message);
--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-const name = " Daniel  ";
+let name = " Daniel  ";
+let nameSpace = name.trim()
+let message = "My name is " + nameSpace + " and my name is " + nameSpace.length + " characters long";
 
 console.log(message);
