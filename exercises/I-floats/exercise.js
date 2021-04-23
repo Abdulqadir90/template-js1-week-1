1b4988a53270a701f5a553277eb6ffedabf6fd48
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-23 14:40:43 +0100

    floats

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let total = numberOfStudents + numberOfMentors;
+let studentsConverted = numberOfStudents/total*100;
+let mentorsConverted = numberOfMentors/total*100;
+
+let percentageStudents = Math.round(studentsConverted);
+// console.log(percentageStudents)
+let percentageMentors = Math.round(mentorsConverted)
+// console.log(percentageMentors)
+
+console.log(`Percentage students: ${percentageStudents} %`)
+console.log(`Percentage mentors: ${percentageMentors} %`)
