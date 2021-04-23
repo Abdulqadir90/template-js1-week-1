f6fe24c8cc9b09fc3e2c5588300ae3fb16fd9372
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-23 16:08:30 +0100

    functions nested done

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,19 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function change(word) {
+    return word.toUppercase();
+
+}
+
+function shoutyGreeting(name) {
+    let greeting = `HELLO ${name}`;
+    return greeting
+}
+
+console.log(shoutyGreeting(mentor1.toUpperCase()))
+console.log(shoutyGreeting(mentor2.toUpperCase()))
+console.log(shoutyGreeting(mentor3.toUpperCase()))
+console.log(shoutyGreeting(mentor4.toUpperCase()))
+console.log(shoutyGreeting(mentor5.toUpperCase()))
