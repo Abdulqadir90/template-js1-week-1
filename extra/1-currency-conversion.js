40da75e3a391f4281a9c6af6b47bd67709187cf7
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-25 22:50:53 +0100

    foreign transactions

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -19,7 +19,9 @@ function convertToUSD(pounds) {
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(pounds) {
+  return pounds * 5.7 * 0.99;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
