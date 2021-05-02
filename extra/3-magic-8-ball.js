de8db478098915df9555784fece69527d1d16e40
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-05-02 19:19:47 +0100

    done but still it more to do

--- extra/3-magic-8-ball.js
+++ extra/3-magic-8-ball.js
@@ -47,11 +47,33 @@
 // and return the answer.
 function shakeBall() {
   //Write your code in here
-  let random = Math.floor(Math.random() * 20);
-  console.log("The ball has SHAKEN")
+  let allAnswers = ["It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",]
 
-  return random;
-  
+    let ranNumbers = Math.random() * allAnswers.length;
+
+    let index = Math.round(ranNumbers);
+    console.log("The Ball has SHAKEN");
+
+    return allAnswers[index];
 }
 
 /* 
